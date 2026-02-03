15b5add64873b8d3d37037b469cabc0b5634f490
Pushkar Lakshakar <139471274+Pushkar-lakshakar@users.noreply.github.com>
2026-02-03 17:22:19 +0530

    add main.dart

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'theme.dart';
+import 'dashboard_page.dart';
+
+void main() {
+  runApp(const CarLeaseApp());
+}
+
+class CarLeaseApp extends StatelessWidget {
+  const CarLeaseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Car Contract Analyzer',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      home: const DashboardPage(),
+    );
+  }
+}
